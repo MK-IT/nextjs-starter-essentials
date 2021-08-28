c7a383e41b497c82c5a34323fab515634d5ef205
Stoyan Merdzhanov <stoyan.merdzhanov@gmail.com>
2021-08-28 21:56:28 +0300

    feat: add `standard-version` and `@commitlint/config-conventional`

---
+++ .versionrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  types: [
+    { type: 'build', section: 'Build' },
+    { type: 'chore', section: 'Chores' },
+    { type: 'ci', section: 'CI' },
+    { type: 'docs', section: 'Docs' },
+    { type: 'feat', section: 'Features' },
+    { type: 'fix', section: 'Bug Fixes' },
+    { type: 'perf', section: 'Performance' },
+    { type: 'refactor', section: 'Refactor' },
+    { type: 'revert', section: 'Revert' },
+    { type: 'style', section: 'Style' },
+    { type: 'test', section: 'Test' }
+  ]
+};
