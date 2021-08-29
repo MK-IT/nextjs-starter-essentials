070b014b3379bf215f09c2bfb23ed9a393548c49
Stoyan Merdzhanov <stoyan.merdzhanov@gmail.com>
2021-08-29 20:30:00 +0300

    feat: add `next-seo` and implement default SEO props for all pages

---
+++ next-seo.config.js
@@ -0,0 +1,38 @@
+/** @type {import('next-seo').DefaultSeoProps} */
+
+const defaultSEOProps = {
+  titleTemplate: "NextJS Starter | %s",
+  defaultTitle: "NextJS Starter By MK IT",
+  description: "NextJS starter bundled with essential tools",
+  additionalMetaTags: [],
+  additionalLinkTags: [
+    {
+      rel: "icon",
+      href: "/images/favicon.ico",
+    },
+  ],
+  twitter: {
+    cardType: "summary_large_image",
+    site: "@twitter-handle",
+    handle: "@twitter-handle",
+  },
+  facebook: {
+    appId: "facebook-app-id",
+  },
+  openGraph: {
+    url: "https://github.com/MK-IT/nextjs-starter-essentials",
+    type: "website",
+    title: "NextJS Starter By MK IT",
+    description: "NextJS starter bundled with essential tools",
+    images: [
+      {
+        url: "/images/default-social-image.png",
+        width: 1024,
+        height: 512,
+        alt: "NextJS Starter By MK IT",
+      },
+    ],
+  },
+};
+
+export default defaultSEOProps;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import type { AppProps } from "next/app";
+import { DefaultSeo } from "next-seo";
 
+import defaultSEOProps from "@root/next-seo.config";
 import "@styles/globals.css";
 import Page from "@layouts/Page";
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <Page>
+      <DefaultSeo {...defaultSEOProps} />
       <Component {...pageProps} />
     </Page>
   );
