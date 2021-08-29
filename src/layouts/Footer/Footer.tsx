6d7489304f7f67318da66ca5685ee688177710a7
Stoyan Merdzhanov <stoyan.merdzhanov@gmail.com>
2021-08-29 19:37:12 +0300

    feat: implement sample Footer component

---
+++ src/layouts/Footer/Footer.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import type { FC } from "react";
+
+import styles from "./Footer.module.css";
+
+const Footer: FC = () => {
+  return (
+    <footer className={styles.footer}>
+      © {new Date().getFullYear()}, Built with
+      {` `}
+      <a
+        href="https://www.nextjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        NextJS
+      </a>
+      {` | `}
+      Powered By{" "}
+      <a href="https://mkit.io" target="_blank" rel="noopener noreferrer">
+        MK IT
+      </a>
+      {` | `}
+      <a
+        href="https://github.com/MK-IT/nextjs-starter-essentials"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        GitHub
+      </a>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import type { NextPage } from "next";
 import Head from "next/head";
-import Image from "next/image";
 
 import styles from "@styles/Home.module.css";
 
@@ -54,19 +53,6 @@ const Home: NextPage = () => {
           </a>
         </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   );
 };
