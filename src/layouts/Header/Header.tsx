235151ed4d53b9562d5676874f14c317b1cf4d02
Stoyan Merdzhanov <stoyan.merdzhanov@gmail.com>
2021-08-29 20:52:00 +0300

    style: retouch Home page and Header component

--- src/layouts/Header/Header.tsx
+++ src/layouts/Header/Header.tsx
@@ -8,13 +8,13 @@ const Header: FC = () => {
   return (
     <header className={styles.header}>
       <div className={styles.header__container}>
-        <h1 className={styles.header__container__title}>
+        <h2 className={styles.header__container__title}>
           <Link href="/">
             <a className={styles.header__container__title__link}>
               NextJS Starter Essentials
             </a>
           </Link>
-        </h1>
+        </h2>
       </div>
     </header>
   );
