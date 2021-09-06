494b576ea9a389ba205d7a2e13c11c59c9d03ab6
Stoyan Merdzhanov <stoyan.merdzhanov@gmail.com>
2021-09-06 18:52:07 +0300

    feat: add AirBnB ESLint configuration including TypeScript support

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,12 @@
 module.exports = {
-  extends: ["next/core-web-vitals", "prettier"],
+  extends: [
+    "airbnb",
+    "airbnb/hooks",
+    "airbnb-typescript",
+    "plugin:@next/next/recommended",
+    "prettier",
+  ],
+  parserOptions: {
+    project: "./tsconfig.json",
+  },
 };
--- src/layouts/Header/Header.tsx
+++ src/layouts/Header/Header.tsx
@@ -4,20 +4,18 @@ import Link from "next/link";
 
 import styles from "./Header.module.css";
 
-const Header: FC = () => {
-  return (
-    <header className={styles.header}>
-      <div className={styles.header__container}>
-        <h2 className={styles.header__container__title}>
-          <Link href="/">
-            <a className={styles.header__container__title__link}>
-              NextJS Starter Essentials
-            </a>
-          </Link>
-        </h2>
-      </div>
-    </header>
-  );
-};
+const Header: FC = () => (
+  <header className={styles.header}>
+    <div className={styles.header__container}>
+      <h2 className={styles.header__container__title}>
+        <Link href="/">
+          <a className={styles.header__container__title__link}>
+            NextJS Starter Essentials
+          </a>
+        </Link>
+      </h2>
+    </div>
+  </header>
+);
 
 export default Header;
