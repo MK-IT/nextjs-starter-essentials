bc8589e54ffc366ad6af5c996f224576a0a812a7
Stoyan Merdzhanov <stoyan.merdzhanov@gmail.com>
2022-04-22 02:06:29 +0300

    fix: viewport for responsive design and `<Page />` wrapper

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable react/jsx-props-no-spreading */
+import Head from "next/head";
 import { DefaultSeo } from "next-seo";
 import type { ReactElement, ReactNode } from "react";
 import type { AppProps } from "next/app";
@@ -19,10 +20,16 @@ function App({ Component, pageProps }: AppPropsWithLayout) {
   // Use the layout defined at the page level, if available
   const getLayout = Component.getLayout ?? ((page) => page);
 
-  return getLayout(
+  return (
     <>
+      <Head>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
       <DefaultSeo {...defaultSEOProps} />
-      <Component {...pageProps} />
+      {getLayout(<Component {...pageProps} />)}
     </>
   );
 }
