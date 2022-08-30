aecabd644df5e1539bd531831bd2bd6b3d4b79ff
Stoyan Merdzhanov <stoyan.merdzhanov@gmail.com>
2022-08-31 01:07:50 +0300

    refactor: .eslintrc.js to match latest usage from Workellence
    - Remove duplicate extends statement
    - Move prettier as last extend to avoid action overriding or conflicts
    - Disallow JSX props spreading
    - Do not throw lint error in stories because of extra dependencies
    - Format the file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,45 @@
 module.exports = {
-  extends: ["airbnb", "airbnb/hooks", "airbnb-typescript", "plugin:@next/next/recommended", "plugin:react/jsx-runtime", "prettier", "plugin:react/jsx-runtime", "plugin:storybook/recommended"],
+  extends: [
+    "airbnb",
+    "airbnb/hooks",
+    "airbnb-typescript",
+    "plugin:@next/next/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:storybook/recommended",
+    "prettier",
+  ],
   plugins: ["testing-library"],
   parserOptions: {
-    project: "./tsconfig.json"
+    project: "./tsconfig.json",
   },
-  overrides: [// Only uses Testing Library lint rules in test files
-  {
-    files: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)"],
-    extends: ["plugin:testing-library/react"]
-  }],
+  overrides: [
+    // Only uses Testing Library lint rules in test files
+    {
+      files: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)"],
+      extends: ["plugin:testing-library/react"],
+    },
+  ],
   // silence TS lint error on JS files
-  ignorePatterns: [".eslintrc.js", "jest.config.js", "jest.setup.js", "next.config.js", "next-sitemap.config.js"],
+  ignorePatterns: [
+    ".eslintrc.js",
+    "jest.config.js",
+    "jest.setup.js",
+    "next.config.js",
+    "next-sitemap.config.js",
+  ],
   rules: {
-    // avoid props spreading or silence on specific lines of code with `// eslint-disable-line`
-    "react/jsx-props-no-spreading": "warn"
-  }
+    // override the rule defaults from extensions to allow devDependencies imports in Storybook stories
+    "import/no-extraneous-dependencies": [
+      "error",
+      {
+        devDependencies: [
+          "**/__tests__/**/*.[jt]s?(x)",
+          "**/?(*.)+(spec|test).[jt]s?(x)",
+          "**/*.stories.*",
+          "**/.storybook/**/*.*",
+        ],
+        peerDependencies: true,
+      },
+    ],
+  },
 };
